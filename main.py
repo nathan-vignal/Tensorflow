677466b30340bb005cb5e3f3b47162886ff1815e
nathan vignal <37573225+nathan-vignal@users.noreply.github.com>
2019-02-01 10:36:50 +0100

    apprentissage et test du modèle

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-#print(tf.__version__)
+# print(tf.__version__)
 
 
 nomHabit = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+            'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 habitMnist = keras.datasets.fashion_mnist
 
-(imageEntrainement, labelEntrainement),(imageTest, labelTest) = habitMnist.load_data()
+(imageEntrainement, labelEntrainement), (imageTest, labelTest) = habitMnist.load_data()
+# output array is read only
+imageEntrainement = imageEntrainement / 255
+imageTest = imageTest / 255
 
 '''
 plt.figure(figsize=(100,100))
@@ -24,12 +27,35 @@ for i in range (0,100):
     plt.imshow(imageEntrainement[i])
 plt.show()'''
 
-
 model = keras.Sequential([
     keras.layers.Flatten(input_shape=(28, 28)),
     keras.layers.Dense(128, activation=tf.nn.relu),
     keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
+
+model.compile(optimizer="adam",
+              loss="sparse_categorical_crossentropy",
+              metrics=["accuracy"])
+
+model.fit(imageEntrainement, labelEntrainement, epochs=5, batch_size=20)
+
+test_loss, test_acc = model.evaluate(imageTest,
+                                     labelTest)  # evaluate(x=None, y=None, batch_size=None, verbose=1, sample_weight=None, steps=None)
+# avec x test data et y test labels
+
+print('Test accuracy:', test_acc)
+predictions = model.predict(imageTest)
+print(predictions[0])
+print(labelTest[0])
+'''
+def plot_image(i, predictions_array, true_label, img):
+    predictions_array = predictions_array[i]
+    true_label = true_label[i]
+    img = img[i]
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img)'''
 
 
 
